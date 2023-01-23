14ddead202dd4bd03381d755b3558cbd148e0ab0
AndreaHerrera20034 <andreaherrera20034@gmail.com>
2023-01-22 22:18:01 -0600

    Actividades

---
+++ navigation_basics/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_basics/route/FirstRoute.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
